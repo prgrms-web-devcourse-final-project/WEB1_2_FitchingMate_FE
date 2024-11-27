9eac6bc20e5facd8053b76accafaefbcb9158ca8
Builter <builter251@gmail.com>
2024-11-27 17:55:56 +0900

    💄 Design : 팀 선택 버튼 패딩 추가

--- src/pages/MyTeamPage/style.ts
+++ src/pages/MyTeamPage/style.ts
@@ -2,4 +2,6 @@ import styled from 'styled-components'
 
 export const MyTeamPageContainer = styled.div``
 
-export const TeamSelectWrapper = styled.div``
+export const TeamSelectWrapper = styled.div`
+  padding: 16px 0 16px 20px;
+`
