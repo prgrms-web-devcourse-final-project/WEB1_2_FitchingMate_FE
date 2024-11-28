709aa3f03646f31a495b99dd480debc4a955e77c
w-ho-choo <ettoprivate@gmail.com>
2024-11-28 17:46:27 +0900

    ✨ Feat : 프로필 사진 업로드시 바뀌게끔 기능구현, 팀선택 하단 모달 기능구현 #78

--- src/pages/ProfilePage/ProfileEdit.tsx
+++ src/pages/ProfilePage/ProfileEdit.tsx
@@ -7,19 +7,49 @@ import {
 
 import Camera from '@assets/icon/camera.svg?react'
 import Select from '@assets/icon/down.svg?react'
-import { useState } from 'react'
+import { useRef, useState } from 'react'
+import BottomModal from '@components/BottomModal'
+import TeamSelectModal from '@components/TeamSelectModal'
+import useTeamDialog from '@hooks/useTeamDialog'
+import BottomModalOption from './BottomModalOption'
 
 const ProfileEdit = () => {
+  const ref = useRef<HTMLDivElement>(null)
+  const [isUpload, setIsUpload] = useState(false)
+  const [profileImg, setProfileImg] = useState<string | undefined>(undefined)
   const [textareaValue, setTextareaValue] = useState('')
+
+  const {
+    selectedTeam,
+    bottomModalRef,
+    handleClickSelectButton,
+    handleTeamSelect,
+  } = useTeamDialog()
+
+  const onImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { files } = e.target
+    if (files) {
+      let image = window.URL.createObjectURL(files[0])
+      setProfileImg(image)
+      setIsUpload(true)
+    }
+  }
 
   return (
     <form>
       <ProfileImageEditWrap>
         <ProfileImageEdit htmlFor='edit_img'>
-          <img
-            src='https://entertainimg.kbsmedia.co.kr/cms/uploads/PERSON_20240206075441_1b54f931528a9d36c98db236a5e19d74.jpg'
-            alt=''
-          />
+          {!isUpload ? (
+            <img
+              src='https://entertainimg.kbsmedia.co.kr/cms/uploads/PERSON_20240206075441_1b54f931528a9d36c98db236a5e19d74.jpg'
+              alt=''
+            />
+          ) : (
+            <img
+              src={profileImg}
+              alt=''
+            />
+          )}
           <div>
             <Camera />
           </div>
@@ -30,6 +60,9 @@ const ProfileEdit = () => {
           id='edit_img'
           style={{ display: 'none' }}
           accept='image/gif, image/jpeg, image/png'
+          onChange={(e) => {
+            onImageUpload(e)
+          }}
         />
       </ProfileImageEditWrap>
       <Form fieldList={[{ name: '닉네임', placeholder: '빌터' }]}></Form>
@@ -38,6 +71,7 @@ const ProfileEdit = () => {
         <textarea
           name=''
           id='edit_notice'
+          value={textareaValue}
           onChange={(e) => {
             setTextareaValue(e.target.value)
           }}
@@ -46,11 +80,15 @@ const ProfileEdit = () => {
 
       <ProfileEditInputWrap>
         <label>마이 팀</label>
-        <div>
-          <span>삼성 라이온즈</span>
+        <div onClick={handleClickSelectButton}>
+          <span>{selectedTeam}</span>
           <Select />
         </div>
       </ProfileEditInputWrap>
+
+      <BottomModal ref={bottomModalRef}>
+        <BottomModalOption onClose={handleTeamSelect} />
+      </BottomModal>
     </form>
   )
 }
