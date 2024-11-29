ab2a8e301fd420b63207c503c0c94e1315972666
june960427 <ljh975896@gmail.com>
2024-11-29 10:44:59 +0900

    ♻️ Refactor : useTeamDialog 팀별 teamId 사용하게 리팩토링

--- src/hooks/useTeamDialog.ts
+++ src/hooks/useTeamDialog.ts
@@ -1,7 +1,16 @@
 import { useRef, useState } from 'react'
 
-const useTeamDialog = () => {
-  const [selectedTeam, setSelectedTeam] = useState('')
+interface TeamDialogProps {
+  callback?: (teamId: string) => void
+}
+
+const initialTeamState = {
+  team: '',
+  teamId: '',
+}
+
+const useTeamDialog = ({ callback }: TeamDialogProps) => {
+  const [selectedTeam, setSelectedTeam] = useState(initialTeamState)
 
   const bottomModalRef = useRef<HTMLDialogElement>(null)
 
@@ -9,13 +18,15 @@ const useTeamDialog = () => {
     bottomModalRef.current?.showModal()
   }
 
-  const handleTeamSelect = (team: string) => {
-    setSelectedTeam(team)
+  const handleTeamSelect = (team: string, teamId: string) => {
+    setSelectedTeam({ team, teamId })
+    callback?.(teamId)
     bottomModalRef.current?.close()
   }
 
   return {
-    selectedTeam,
+    selectedTeam: selectedTeam.team,
+    selectedTeamId: selectedTeam.teamId,
     bottomModalRef,
     handleClickSelectButton,
     handleTeamSelect,
