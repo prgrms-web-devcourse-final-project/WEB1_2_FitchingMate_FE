74772a5dce68a58f0d66601c2bb2a711ee9996f7
june960427 <ljh975896@gmail.com>
2024-12-01 04:40:56 +0900

    ♻️ Refactor : handler props로 받을때 on으로 수정, 이미지 랜더링 createObjectURL로 수정

--- src/pages/GoodsPostingPage/Tabs/FirstTab/ImageSection/ImageCard/index.tsx
+++ src/pages/GoodsPostingPage/Tabs/FirstTab/ImageSection/ImageCard/index.tsx
@@ -3,33 +3,35 @@ import { ImageCardContainer } from './style'
 interface ImageCardProps {
   image: File
   index: number
-  handleDeleteImage: (index: number) => void
-  handleDragDrop: (e: React.DragEvent, index: number) => void
-  handleDragStart: (e: React.DragEvent<HTMLDivElement>, id: number) => void
-  handleDragOver: (e: React.DragEvent<HTMLDivElement>) => void
-  handleDragEnd: (e: React.DragEvent<HTMLDivElement>) => void
+  oneDeleteImage: (index: number) => void
+  onDragDrop: (e: React.DragEvent, index: number) => void
+  onDragStart: (e: React.DragEvent<HTMLDivElement>, id: number) => void
+  onDragOver: (e: React.DragEvent<HTMLDivElement>) => void
+  onDragEnd: (e: React.DragEvent<HTMLDivElement>) => void
 }
 
 const ImageCard = ({
   image,
   index,
-  handleDeleteImage,
-  handleDragDrop,
-  handleDragStart,
-  handleDragOver,
-  handleDragEnd,
+  oneDeleteImage,
+  onDragDrop,
+  onDragStart,
+  onDragOver,
+  onDragEnd,
 }: ImageCardProps) => {
+  const formatImage = URL.createObjectURL(image)
+
   return (
     <ImageCardContainer>
       <img
-        src={URL.createObjectURL(image)}
+        src={formatImage}
         draggable
-        onDragStart={(e) => handleDragStart(e, index)}
-        onDrop={(e) => handleDragDrop(e, index)}
-        onDragOver={(e) => handleDragOver(e)}
-        onDragEnd={(e) => handleDragEnd(e)}
+        onDragStart={(e) => onDragStart(e, index)}
+        onDrop={(e) => onDragDrop(e, index)}
+        onDragOver={(e) => onDragOver(e)}
+        onDragEnd={(e) => onDragEnd(e)}
       />
-      <button onClick={() => handleDeleteImage(index)}>X</button>
+      <button onClick={() => oneDeleteImage(index)}>X</button>
     </ImageCardContainer>
   )
 }
--- src/pages/GoodsPostingPage/Tabs/FirstTab/ImageSection/index.tsx
+++ src/pages/GoodsPostingPage/Tabs/FirstTab/ImageSection/index.tsx
@@ -4,18 +4,11 @@ import ImageCard from './ImageCard'
 import ImageInput from '@components/ImageInput'
 import useImageChange from '@hooks/useImageChange'
 
-const ImageSection = () => {
-  const MAX_IMAGE_COUNT = 15
+const MAX_IMAGE_COUNT = 15
 
-  const {
-    imageList,
-    handleChangeImage,
-    handleDeleteImage,
-    onDragDrop,
-    onDragStart,
-    onDragOver,
-    onDragEnd,
-  } = useImageChange(MAX_IMAGE_COUNT)
+const ImageSection = () => {
+  const { imageList, onChangeImage, onDeleteImage, ...restDragHandler } =
+    useImageChange(MAX_IMAGE_COUNT)
 
   const inputDisabled = imageList.length >= MAX_IMAGE_COUNT
 
@@ -24,7 +17,7 @@ const ImageSection = () => {
       <ImageInput
         maxCount={MAX_IMAGE_COUNT}
         currentCount={imageList.length}
-        onChange={handleChangeImage}
+        onChangeImage={onChangeImage}
         disabled={inputDisabled}
         multiple
       />
@@ -35,11 +28,8 @@ const ImageSection = () => {
             key={`${image.name}-${index}`}
             image={image}
             index={index}
-            handleDragDrop={onDragDrop}
-            handleDragStart={onDragStart}
-            handleDragOver={onDragOver}
-            handleDeleteImage={handleDeleteImage}
-            handleDragEnd={onDragEnd}
+            {...restDragHandler}
+            oneDeleteImage={onDeleteImage}
           />
         ))}
       </ImageList>
