566dd1495a7bb6e72ef181ec21da25037d926d8c
june960427 <ljh975896@gmail.com>
2024-12-02 14:44:10 +0900

    ✨ Feat : 페이지별 플롯버튼 생성 및 포스팅 페이지로 경로 처리 완료

--- src/layouts/GlobalLayout/index.tsx
+++ src/layouts/GlobalLayout/index.tsx
@@ -1,28 +1,17 @@
 import Header from '@layouts/Header'
 import GlobalNav from '@layouts/GlobalNav'
 import { Outlet } from 'react-router-dom'
-import {
-  GlobalContainer,
-  GlobalFloatAside,
-  GlobalFloatButton,
-} from '@styles/globalStyle'
-
-import Write from '@assets/icon/write.svg?react'
+import { GlobalContainer } from '@styles/globalStyle'
 
 const GlobalLayout = () => {
   return (
-    <div>
+    <>
       <Header />
       <GlobalContainer>
         <Outlet />
       </GlobalContainer>
-      <GlobalFloatAside>
-        <GlobalFloatButton>
-          <Write />
-        </GlobalFloatButton>
-      </GlobalFloatAside>
       <GlobalNav />
-    </div>
+    </>
   )
 }
 
