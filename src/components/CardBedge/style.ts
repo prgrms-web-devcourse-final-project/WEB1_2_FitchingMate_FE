c6eecba7ad0d1688c6742a734fa06cc5943bc727
Builter <builter251@gmail.com>
2024-12-03 22:41:27 +0900

    💄 메이트 카드 요소 수정

--- src/components/CardBedge/style.ts
+++ src/components/CardBedge/style.ts
@@ -5,12 +5,18 @@ interface BedgeProps {
 }
 
 export const Bedge = styled.div<BedgeProps>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
   width: fit-content;
-  height: fit-content;
-  padding: 0.25em 0.625em;
+  height: 1.5em; /* 고정 높이 */
+  padding: 0 0.625em; /* 텍스트 주변의 여백 */
   color: ${({ theme }) => theme.fontColor.white};
   background-color: ${({ $bgcolor, theme }) =>
     $bgcolor || theme.fontColor.navy};
   border-radius: 4px;
   font-size: ${({ theme }) => theme.fontSize.medium};
+  white-space: nowrap; /* 텍스트를 한 줄로 유지 */
+  overflow: hidden;
+  text-overflow: ellipsis;
 `
