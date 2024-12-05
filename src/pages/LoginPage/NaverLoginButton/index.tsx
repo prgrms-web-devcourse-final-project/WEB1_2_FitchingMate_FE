ae6f6eb459b702d8f02a3bc4ba674c57957077dd
Builter <builter251@gmail.com>
2024-12-05 15:23:56 +0900

    ✨ Feat : 네이버 로그인 URL 연결
    네이버 로그인 성공 후 리다이렉트 확인 필요

--- src/pages/LoginPage/NaverLoginButton/index.tsx
+++ src/pages/LoginPage/NaverLoginButton/index.tsx
@@ -1,33 +1,37 @@
+import React from 'react'
 import { StyledButton } from './style'
 import NaverButton from '@assets/icon/naver_login.png'
 
 // UUID 생성 함수: CSRF 방지를 위한 고유 state 값 생성
 const generateUUID = (): string => {
-  return ([1e7] as any + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c: string) =>
-    (
-      parseInt(c, 10) ^
-      (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (parseInt(c, 10) / 4)))
-    ).toString(16)
+  return (([1e7] as any) + -1e3 + -4e3 + -8e3 + -1e11).replace(
+    /[018]/g,
+    (c: any) =>
+      (
+        parseInt(c, 10) ^
+        (crypto.getRandomValues(new Uint8Array(1))[0] &
+          (15 >> (parseInt(c, 10) / 4)))
+      ).toString(16),
   )
 }
 
 const NaverLoginButton = () => {
-  // 환경 변수 가져오기
-  const CLIENT_ID = import.meta.env.VITE_NAVER_CLIENT_ID
-  const REDIRECT_URI = encodeURIComponent(import.meta.env.VITE_REDIRECT_URI as string)
   const STATE = generateUUID()
 
   // 네이버 로그인 인증 URL 생성
-  const NAVER_AUTH_URL = `https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&state=${STATE}`
+  const NAVER_AUTH_URL = `${import.meta.env.VITE_API_ENDPOINT}/auth/connect/naver?state=${STATE}`
 
-  // 로그인 버튼 클릭 핸들러
-  const handleLoginClick = () => {
+  const handleClick = () => {
+    // 네이버 인증 URL로 리다이렉트
     window.location.href = NAVER_AUTH_URL
   }
 
   return (
-    <StyledButton onClick={handleLoginClick}>
-      <img src={NaverButton} alt="네이버 로그인" />
+    <StyledButton onClick={handleClick} type='button'>
+      <img
+        src={NaverButton}
+        alt='네이버 로그인'
+      />
     </StyledButton>
   )
 }
