5928d46c1b1f7b8cc98ac990b66f10f81d4bfb91
Builter <builter251@gmail.com>
2024-12-06 00:44:31 +0900

    ✨ 네이버 로그인 및 회원가입 성공

--- src/pages/LoginPage/AuthCallback/NaverCallback.tsx
+++ src/pages/LoginPage/AuthCallback/NaverCallback.tsx
@@ -1,36 +1,49 @@
-import { useEffect } from 'react';
-import fetchApi from '@apis/ky';
+import { useEffect } from 'react'
+import fetchApi from '@apis/ky'
+import { NaverLoginResponse } from '@typings/db'
+import { useNavigate } from 'react-router-dom'
+import { ROUTE_PATH } from '@constants/ROUTE_PATH'
 
 const NaverCallback = () => {
+  const navigate = useNavigate()
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         // URL에서 쿼리 파라미터 추출
-        const urlParams = new URLSearchParams(window.location.search);
-        const code = urlParams.get('code');
-        const state = urlParams.get('state');
+        const urlParams = new URLSearchParams(window.location.search)
+        const code = urlParams.get('code')
+        const state = urlParams.get('state')
 
         if (code && state) {
           // 네이버 로그인 데이터 확인용 요청
-          const data = await fetchApi
+          const response = await fetchApi
             .get(`auth/login/naver`, {
               searchParams: { code, state },
             })
-            .json();
+            .json<NaverLoginResponse>()
 
-          console.log('네이버 로그인 응답:', data);
+          console.log('네이버 로그인 응답:', response)
+
+          if (response.isNewMember) {
+            navigate(ROUTE_PATH.SIGNUP, {
+              state: { profile: response.naverProfileResponse },
+            })
+          } else {
+            // 추후 로그인 처리
+          }
         } else {
-          console.error('code 또는 state가 누락되었습니다.');
+          console.error('code 또는 state가 누락되었습니다.')
         }
       } catch (error) {
-        console.error('네이버 로그인 처리 중 오류가 발생했습니다.', error);
+        console.error('네이버 로그인 처리 중 오류가 발생했습니다.', error)
       }
-    };
+    }
 
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
-  return <div>네이버 로그인 처리 중...</div>;
-};
+  return
+}
 
-export default NaverCallback;
+export default NaverCallback
--- src/pages/LoginPage/SignupPage/index.tsx
+++ src/pages/LoginPage/SignupPage/index.tsx
@@ -1,3 +1,4 @@
+import { useLocation, useNavigate } from 'react-router-dom'
 import Form from '@components/Form'
 import {
   SignupPageContainer,
@@ -8,37 +9,89 @@ import {
 } from './style'
 import TeamSelectModal from '@components/TeamSelectModal'
 import GlobalButton from '@components/GlobalButton'
-import { useNavigate } from 'react-router-dom'
+import fetchApi from '@apis/ky'
+import { ROUTE_PATH } from '@constants/ROUTE_PATH'
+import { useState } from 'react'
 
 const SignupPage = () => {
+  const location = useLocation()
   const navigate = useNavigate()
+  const profile = location.state?.profile
+  const [selectedTeamId, setSelectedTeamId] = useState<number | undefined>(
+    undefined,
+  )
+  const [nickname, setNickname] = useState('')
 
-  const infoFieldList = [
-    { name: '닉네임', placeholder: '닉네임을 입력해주세요.' },
-  ]
+  if (!profile) {
+    console.error('프로필 정보가 누락되었습니다.')
+    return <div>프로필 정보를 로드하는 데 문제가 발생했습니다.</div>
+  }
+  const isButtonEnabled = nickname.trim() !== '' && selectedTeamId !== undefined
+  const handleSignup = async () => {
+    if (!isButtonEnabled) return
+
+    try {
+      // 회원가입 API 호출
+      const response = await fetchApi.post('members/join', {
+        json: {
+          name: profile.name,
+          email: profile.email,
+          gender: profile.gender,
+          birthyear: profile.birthyear,
+          nickname: nickname,
+          teamId: selectedTeamId,
+        },
+      })
+
+      // 가입 완료 후 메인 페이지로 이동
+      navigate(ROUTE_PATH.HOME)
+
+      // UX를 위해 두 가지 방법 고민
+      // 1.toastify를 이요
+    } catch (error) {
+      
+      console.error('회원가입 처리 중 오류가 발생했습니다.', error)
+    }
+  }
 
   return (
     <SignupPageContainer>
       <FieldWrapper>
         <StyledLabel>연령대 / 성별</StyledLabel>
-        <p>20대 / 남</p>
+        <p>
+          {profile.birthyear} / {profile.gender === 'M' ? '남' : '여'}
+        </p>
       </FieldWrapper>
 
       <StyledForm>
-        <Form fieldList={infoFieldList} />
+        <Form
+          fieldList={[
+            { name: '닉네임', placeholder: '닉네임을 입력해주세요.' },
+          ]}
+          onChange={(fieldName, value) => {
+            if (fieldName === '닉네임') {
+              setNickname(value)
+              console.log('닉네임: ', value)
+            }
+          }}
+        />
         <FieldWrapper>
           <StyledLabel>마이팀</StyledLabel>
-          <TeamSelectModal />
+          <TeamSelectModal
+            onTeamSelect={(team) => {
+              setSelectedTeamId(team.teamId)
+              console.log('선택한 팀 id: ', team.teamId)
+            }}
+          />
         </FieldWrapper>
       </StyledForm>
 
       <GlobalButtonWrapper>
         <GlobalButton
           text='회원가입'
-          $isNavy={true}
-          onClick={() => {
-            navigate('/')
-          }}
+          $isNavy={isButtonEnabled}
+          $disabled={!isButtonEnabled} 
+          onClick={handleSignup}
         />
       </GlobalButtonWrapper>
     </SignupPageContainer>
