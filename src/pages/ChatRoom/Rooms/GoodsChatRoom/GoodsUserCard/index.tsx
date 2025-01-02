d07c27ab2488e25da32d741fa0f0afd148a44d29
w-ho-choo <ettoprivate@gmail.com>
2025-01-02 13:34:42 +0900

    ✨ Feat : 채팅방 유저 클릭시 유저 프로필 이동 기능 추가

--- src/pages/ChatRoom/Rooms/GoodsChatRoom/GoodsUserCard/index.tsx
+++ src/pages/ChatRoom/Rooms/GoodsChatRoom/GoodsUserCard/index.tsx
@@ -1,6 +1,7 @@
 import ProfileBedge from '@components/ProfileBedge'
 import { UserInfo, UserListCardContainer } from './style'
 import { useGoodsChatStore } from '@store/useGoodsChatStore'
+import { Link } from 'react-router-dom'
 
 interface GoodsUserCardProps {
   user: {
@@ -17,20 +18,22 @@ const GoodsUserCard = ({ user }: GoodsUserCardProps) => {
   const isSeller = memberId === currentSellerId
 
   return (
-    <UserListCardContainer>
-      <UserInfo>
-        <ProfileBedge
-          height={3}
-          width={3}
-          imageSrc={imageUrl}
-          isChat={true}
-        />
-        <p>
-          {nickname}
-          {isSeller && <span>(판매자)</span>}
-        </p>
-      </UserInfo>
-    </UserListCardContainer>
+    <Link to={`/profile/${memberId}`}>
+      <UserListCardContainer>
+        <UserInfo>
+          <ProfileBedge
+            height={3}
+            width={3}
+            imageSrc={imageUrl}
+            isChat={true}
+          />
+          <p>
+            {nickname}
+            {isSeller && <span>(판매자)</span>}
+          </p>
+        </UserInfo>
+      </UserListCardContainer>
+    </Link>
   )
 }
 
